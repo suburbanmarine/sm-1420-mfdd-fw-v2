2676013c1c8ed715bb141acbf9fe9bc5d7cba11c
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-08-17 16:47:34 -0700

    fix build flags

--- Toolchain_STM32H753VIT.cmake
+++ Toolchain_STM32H753VIT.cmake
@@ -18,27 +18,31 @@ set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
 
-set( ARCH_STM32 "-mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb")
+set( ARCH_STM32 "-mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard")
 set( FLAGS_STM32 "${ARCH_STM32} -ffunction-sections -fdata-sections -fstrict-volatile-bitfields" )
 
 set( WARN_STM32 "-Wall -Werror=return-type" )
 
 set(CMAKE_ASM_FLAGS 				"${FLAGS_STM32} ${WARN_STM32} -Wa,--warn -x assembler-with-cpp" CACHE STRING "" FORCE)
-set(CMAKE_ASM_FLAGS_DEBUG 			"-Og -g3" CACHE STRING "" FORCE)
+set(CMAKE_ASM_FLAGS_DEBUG 			"-O0 -g3" CACHE STRING "" FORCE)
 set(CMAKE_ASM_FLAGS_RELEASE 		"-O1" CACHE STRING "" FORCE)
 set(CMAKE_ASM_FLAGS_RELWITHDEBINFO 	"-O1 -g3" CACHE STRING "" FORCE)
 set(CMAKE_ASM_FLAGS_MINSIZEREL 		"-O1" CACHE STRING "" FORCE)
 
 set(CMAKE_C_FLAGS 					"${FLAGS_STM32} ${WARN_STM32} -fstack-usage" CACHE STRING "" FORCE)
-set(CMAKE_C_FLAGS_DEBUG 			"-Og -g3" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG 			"-O0 -g3" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_RELEASE 			"-Os" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_RELWITHDEBINFO 	"-Os -g3" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS_MINSIZEREL 		"-Os" CACHE STRING "" FORCE)
 
 set(CMAKE_CXX_FLAGS 				"${FLAGS_STM32} ${WARN_STM32} -fstack-usage -fno-threadsafe-statics -fno-exceptions -fno-rtti -fno-use-cxa-atexit" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS_DEBUG 			"-Og -g3" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG 			"-O0 -g3" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE 		"-Os" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO 	"-Os -g3" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS_MINSIZEREL 		"-Os" CACHE STRING "" FORCE)
 
-set(CMAKE_EXE_LINKER_FLAGS "${ARCH_STM32} -T\"${CMAKE_CURRENT_SOURCE_DIR}/STM32H753VITx_FLASH.ld\" -u _printf_float -Wl,-cref,-u,Reset_Handler --specs=nosys.specs" CACHE STRING "" FORCE)
+set(CMAKE_C_LINK_FLAGS "${ARCH_STM32} -T\"${PROJECT_SOURCE_DIR}/STM32H753VITx_FLASH.ld\" --specs=nano.specs -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--print-memory-usage" CACHE STRING "" FORCE)
+
+# cmake does not know how to find the syscalls ST has in syscalls.c, so cannot check the compiler
+set(CMAKE_C_COMPILER_WORKS   1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
